65a8d19ec742c516d60fab0bd8905bdf51a5a3f8
Vitalia Solomina <vitalia.podgurskaya@aldigit.com>
2014-10-15 15:14:13 +0400

    Fixed indentations

--- activeNavigation.js
+++ activeNavigation.js
@@ -1,15 +1,14 @@
 (function( $ ) {
-	$.fn.activeNavigation = function(selector) {
-		var pathname = window.location.pathname
-		var hrefs = []
-    	$(selector).find("a").each(function(){
-    		if (pathname.indexOf($(this).attr("href")) > -1) 
-    			hrefs.push($(this))
-    	 })
-
-	    hrefs.sort(function(a,b){
-	    	return a.attr("href").length < b.attr("href").length
-	    })
-	    hrefs[0].addClass("active")
-	};
+    $.fn.activeNavigation = function(selector) {
+        var pathname = window.location.pathname
+        var hrefs = []
+        $(selector).find("a").each(function(){
+            if (pathname.indexOf($(this).attr("href")) > -1) 
+                hrefs.push($(this))
+        })
+        hrefs.sort(function(a,b){
+            return a.attr("href").length < b.attr("href").length
+        })
+        hrefs[0].addClass("active")
+    };
 })(jQuery);
